e33c751a88d2cc0d6bda8e0d36e16902f3c55c92
Akhil Varma <akhilvarma2378@gmail.com>
2025-11-29 13:41:22 +0530

    defined schema and db setup done

---
+++ backend/db.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI;)
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = {
+    User
+}
